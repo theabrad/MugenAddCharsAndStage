1b14679cf54cd12258945ac7141e8d233944c9fd
Alex Bradley <alex.guerrilla@gmail.com>
2012-06-11 21:31:32 -0400

    removed wx methods for now

--- addChars_and_Stage.py
+++ addChars_and_Stage.py
@@ -11,27 +11,6 @@ stageDir = directory + '\\stages'				#Folder for stages
 selectDir = directory + '\\data'				#Folder for select.def
 musicDir = directory + '\\sound'				#Folder for Music
 
-class MainWindow(wx.Frame):
-	def __init__(self, parent, title):
-		wx.Frame.__init__(self,parent,title=title, size=(640,480))
-		self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-		self.CreateStatusBar()
-
-		#Setting up the menu
-		filemenu = wx.Menu()
-
-		filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
-		filemenu.AppendSeperator()
-		filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
-
-		#Creating the menubar
-		menuBar = wx.MenuBar()
-		menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        self.Show(True)
-
-
-
 
 #get the list of characters
 def getChars():
@@ -58,42 +37,50 @@ def addChar(character, stage):
 	else:
 		file.write("{0}, stages/{1}\n".format(character,stage))
 
-#change to select def directory
-os.chdir(selectDir)
-spaces = '->\n'
-breakLine = ';---------------------------------------------------------------------\n\n\n'
+def addStages(stage):
+	file.write("stages/{0}\n".format(stage))
+
+def addMusicToStage(stage):
+	f = open("{0}.def".format(stage),"r+")
+	f.close()
+
+
+
 
 #get chars and stages
 charList = getChars()
 stageName - getStages()
 
 #write the list of characters to a text file
-file = open(select, "w")
-file.write(breakLine)
-file.write('[Characters]\n')
-file.write('randomselect\n')
-for item in charList: #place in addChar
-	if item == 'MVC2_DrDoom':
-		file.write("%s, stages/drdoombg.def\n" %item)
-	else:
-		file.write("%s\n" %item)
+def writeToFile():
+	spaces = '->\n'
+	breakLine = ';---------------------------------------------------------------------\n\n\n'
 
-#write the list of stages
-file.write(breakLine)
-file.write('[ExtraStages]\n')
-for item in stageName:
-    file.write("stages/%s\n" %item)
+	#change to select def directory
+	os.chdir(selectDir)
 
-#write the options
-file.write(breakLine)
-file.write('[Options]\n')
-file.write('arcade.maxmatches = 6,1,1,0,0,0,0,0,0,0\n')
-file.write('team.maxmatches = 4,1,1,0,0,0,0,0,0,0\n')
+	file = open(select, "w")
+	file.write(breakLine)
+	file.write('[Characters]\n')
+	file.write('randomselect\n')
+	for item in charList: #place in addChar
+		if item == 'MVC2_DrDoom':
+			file.write("%s, stages/drdoombg.def\n" %item)
+		else:
+			file.write("%s\n" %item)
 
-file.close()
+	#write the list of stages
+	file.write(breakLine)
+	file.write('[ExtraStages]\n')
+	for item in stageName:
+		file.write("stages/%s\n" %item)
 
-#Run the app
-app = wx.App(False)
-frame = MainWindow(None, "M.U.G.E.N Add Characters and Stages")
-app.MainLoop()
+	#write the options
+	file.write(breakLine)
+	file.write('[Options]\n')
+	file.write('arcade.maxmatches = 6,1,1,0,0,0,0,0,0,0\n')
+	file.write('team.maxmatches = 4,1,1,0,0,0,0,0,0,0\n')	
+
+	file.close()
+
 
